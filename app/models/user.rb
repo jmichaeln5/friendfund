27fc46abcd5f9756803dd63b1cde67a49c6a2a63
Jon Norton <jmichaeln5@gmail.com>
2021-07-25 02:11:01 -0400

    Added base friendship migration with model associations(has_many through self reference)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,24 @@ class User < ApplicationRecord
 
   has_person_name
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  validates :first_name, presence: true, length: { minimum: 2, maximum: 100 }
+  validates :last_name, presence: true, length: { minimum: 2, maximum: 100 }
+
+## # https://medium.com/@elizabethprendergast/using-custom-relation-queries-to-establish-friends-and-friendships-in-rails-and-activerecord-6c6e5825433a
+## #
+
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
+
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :inverse_friends, :through => :inverse_friendships, :source => :user
+
+  def active_friends
+    friends.select{ |friend| friend.friends.include?(self) }
+  end
+
+  def pending_friends
+    friends.select{ |friend| !friend.friends.include?(self) }
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_06_135015) do
+ActiveRecord::Schema.define(version: 2021_07_25_052747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friendships", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "friend_id", null: false
+    t.boolean "confirmed"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -43,4 +53,6 @@ ActiveRecord::Schema.define(version: 2021_07_06_135015) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "friendships", "users"
+  add_foreign_key "friendships", "users", column: "friend_id"
 end
