7930c71f63c16fed31919573393f808336e5ad38
Jon Norton <jmichaeln5@gmail.com>
2021-07-31 13:06:40 -0400

    Updating styling for User partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,13 @@ class UsersController < ApplicationController
   # GET /users or /users.json
   def index
     @users = User.all.order("created_at ASC")
+    users = User.all.order("created_at ASC")
   end
 
   # GET /users/1 or /users/1.json
   def show
     @user = User.find(params[:id])
+    user = @user
   end
 
 end
