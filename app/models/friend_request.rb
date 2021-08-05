bbe09b224e5efeb0ea1c9a236f7a7a3cefe3d2e1
Jon Norton <jmichaeln5@gmail.com>
2021-08-04 21:10:08 -0400

    Added check_if_request_exists before_create action in friend_request.rb to forbid creation of exisiting friend requests

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,16 +1,8 @@
 class FriendRequest < ApplicationRecord
-  enum status: { pending: 0, accepted: 1, rejected: 2, canceled: 3 } # Cancel option to be overwritten with soft delete
-  before_create :check_request_receiver
-
-  # ### Both check_request_status && check_request_actor work inconsistently
-  before_save :check_request_status
-  before_save :check_request_actor
-
-  before_destroy :yeet_notification
+  enum status: { pending: 0, accepted: 1, rejected: 2, canceled: 3 } # Cancel option should trigger soft delete
 
   belongs_to :requestor, class_name: :User
   belongs_to :receiver, class_name: :User
-
   ############
   # has_many :users, through: :friend_requests
   # has_many :followers, :class_name => 'User', :through => :friendships, :foreign_key => 'friend_id'
@@ -20,16 +12,17 @@ class FriendRequest < ApplicationRecord
   # has_many :users, class_name: :User, through: self
   ############
 
+  before_create :check_request_status
+  before_create :check_if_request_exists
+  before_destroy :yeet_notification
+
+  validates :status, presence: true
   validate :disallow_self_referential_friendship
 
-  def check_request_actor
-    if (self.class.all.where(requestor_id: self.receiver_id, receiver_id: self.requestor_id ).any?) ==  true
-      self.errors.add(:base, "Invalid Action, Friend Request already exists.")
-      throw(:abort)
-    end
-  end
-
   def check_request_receiver
+    puts "*"*100
+    puts " *** Checking Reqeust Actor *** "*100
+    puts "*"*100
     if (self.class.all.where(receiver_id: self.requestor_id, requestor_id: self.receiver_id ).any? )  ==  true
       self.errors.add(:base, "Invalid Action, Friend Request already exists.")
       throw(:abort)
@@ -57,6 +50,20 @@ class FriendRequest < ApplicationRecord
         errors.add(:base, "Invalid Action.")
       end
     end
+
+
+    def check_if_request_exists
+      if (self.class.all.where(requestor_id: self.requestor_id, receiver_id: self.receiver_id ).any?) ==  true
+        self.errors.add(:base, "Invalid Action, Friend Request already exists.")
+        throw(:abort)
+      end
+
+      if (self.class.all.where(requestor_id: self.receiver_id, receiver_id: self.requestor_id ).any?) ==  true
+        self.errors.add(:base, "Invalid Action, Friend Request already exists.")
+        throw(:abort)
+      end
+    end
+
 
     def check_request_status
       puts "Checking FriendRequest..."
