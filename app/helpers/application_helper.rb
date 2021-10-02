0da8f04647e721b62fea8bb7bf0654ef558e49e4
Jon Norton <jmichaeln5@gmail.com>
2021-10-02 03:09:49 -0400

    remove whitespace

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
   def locale_to
     @locale_to = "#{controller_name.capitalize}##{action_name}"
-  end  
+  end
+
+  def is_nil_and_zero(data)
+     data.blank? || data == 0
+  end
 end
